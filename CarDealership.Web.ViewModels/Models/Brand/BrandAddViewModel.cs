ce9012806bc30b5c170d2686d6e1c7cb4d7876f6
okay osman <okay03@abv.bg>
2024-11-23 22:37:30 +0200

    Add a model added to layour

--- CarDealership.Web.ViewModels/Models/Brand/BrandAddViewModel.cs
+++ CarDealership.Web.ViewModels/Models/Brand/BrandAddViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static CarDealershipApp.Constants.Constants;
 
+using System.Runtime.CompilerServices;
 namespace CarDealership.Web.ViewModels.Models.Brand
-{ 
-    public class BrandAddViewModel
-    {
-        [Required]
-        [MinLength(BrandNameMinLength)]
-        [MaxLength(BrandNameMaxLength)]
-        public string Name { get; set; }
-        [MinLength(ModelNameMinLength)]
-        [MaxLength(ModelNameMaxLength)]
-        public string? Model { get; set; }
+{
+	public class BrandAddViewModel
+	{
+		[Required]
+		[MinLength(BrandNameMinLength)]
 
-        public string? ImageUrl { get; set; }
-    }
+
+		[MaxLength(BrandNameMinLength)]
+
+		public string Name { get; set; }
+		[MinLength(ModelNameMinLength)]
+		[MaxLength(ModelNameMaxLength)]
+		public string? Model { get; set; }
+
+
+		public string? ImageUrl { get; set; }
+
+	}
 }
