28aca357de1fe350351dcea446ba7ff817a96a3d
okay osman <okay03@abv.bg>
2024-12-03 18:39:14 +0200

    Model Index view added

---
+++ CarDealership.Web.ViewModels/Models/Model/ModelIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealership.ViewModels.Models.Model
+{
+	public class ModelIndexViewModel
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+    }
+}
