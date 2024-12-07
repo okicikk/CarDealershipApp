fdd6c76ebaba9171a6e47a2cf98dbb8a9b2d8467
okay osman <okay03@abv.bg>
2024-12-07 02:17:46 +0200

    DefaultUser Added

--- CarDealershipApp/Extensions/ApplicationExtensions.cs
+++ CarDealershipApp/Extensions/ApplicationExtensions.cs
@@ -1,47 +1,69 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using static CarDealershipApp.Constants.Constants;
 
 namespace CarDealershipApp.Web.Extensions
 {
-	public static class ApplicationExtensions
-	{
-		public static async Task SeedRolesAndAdminUserAsync(this IApplicationBuilder app)
-		{
-			using (var scope = app.ApplicationServices.CreateScope())
-			{
-				var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-				var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+    public static class ApplicationExtensions
+    {
+        public static async Task SeedRolesAndUsersAsync(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
-				var roles = new[] { "Admin" };
+                var roles = new[] { "Admin", "User" };
 
-				// Seed roles
-				foreach (var role in roles)
-				{
-					if (!await roleManager.RoleExistsAsync(role))
-					{
-						await roleManager.CreateAsync(new IdentityRole(role));
-					}
-				}
+                // Seed roles
+                foreach (var role in roles)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
 
-				// Seed admin user
-				var adminEmail = "admin@gmail.com";
-				var adminPassword = "AdminPass";
+                // Seed admin user
+                string adminEmail = "admin@gmail.com";
+                string adminPassword = "AdminPass";
 
-				var adminUser = await userManager.FindByEmailAsync(adminEmail);
-				if (adminUser == null)
-				{
-					adminUser = new IdentityUser
-					{
-						UserName = adminEmail,
-						Email = adminEmail
-					};
+                // Seed default user for cars with deleted users and for seeding
+                string defaultEmail = DefaultUserEmail;
+                string defaultPass = "DefaultPass";
 
-					var createResult = await userManager.CreateAsync(adminUser, adminPassword);
-					if (createResult.Succeeded)
-					{
-						await userManager.AddToRoleAsync(adminUser, "Admin");
-					}
-				}
-			}
-		}
-	}
+                var adminUser = await userManager.FindByEmailAsync(adminEmail);
+                var defaultUser = await userManager.FindByEmailAsync(defaultEmail);
+                if (adminUser == null)
+                {
+                    adminUser = new IdentityUser
+                    {
+                        Id = "8530b19f-939d-4fd8-b4b0-d778de91ab50",
+                        UserName = adminEmail,
+                        Email = adminEmail
+                    };
+
+                    var createResult = await userManager.CreateAsync(adminUser, adminPassword);
+                    if (createResult.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(adminUser, "Admin");
+                        await userManager.AddToRoleAsync(adminUser, "User");
+                    }
+                }
+                if (defaultUser is null)
+                {
+                    defaultUser = new IdentityUser()
+                    {
+                        Id = "4e32b02a-046c-40be-bfeb-327c900e6bb9",
+                        UserName = defaultEmail,
+                        Email = defaultEmail
+                    };
+                    var createResult = await userManager.CreateAsync(defaultUser, defaultPass);
+                    if (createResult.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(adminUser, "User");
+                    }
+                }
+            }
+        }
+    }
 }
--- CarDealershipApp/Program.cs
+++ CarDealershipApp/Program.cs
@@ -9,68 +9,74 @@ using CarDealershipApp.Infrastructure.Repositories;
 using CarDealershipApp.Web.Extensions;
 namespace CarDealershipApp
 {
-	public class Program
-	{
-		public static async Task Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-			var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
-			builder.Services.AddDbContext<CarDealershipDbContext>(options => options.UseSqlServer(connectionString));
+            builder.Services.AddDbContext<CarDealershipDbContext>(options => options.UseSqlServer(connectionString));
 
 
-			builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-			{
-				options.SignIn.RequireConfirmedAccount = false;
-				options.Password.RequireNonAlphanumeric = false;
-				options.Password.RequireDigit = false;
-			})
-				.AddRoles<IdentityRole>()
-				.AddEntityFrameworkStores<CarDealershipDbContext>();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<CarDealershipDbContext>();
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
-			builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
-			builder.Services.AddScoped<IBrandService, BrandService>();
-			builder.Services.AddScoped<IModelService, ModelService>();
-			builder.Services.AddScoped<ICarService, CarService>();
+            builder.Services.AddScoped<IBrandService, BrandService>();
+            builder.Services.AddScoped<IModelService, ModelService>();
+            builder.Services.AddScoped<ICarService, CarService>();
             builder.Services.AddScoped<IFeatureService, FeatureService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
 
 
 
 
             var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
 
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+              name: "Admin",
+              pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-			app.MapRazorPages();
+            app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
-			await app.SeedRolesAndAdminUserAsync();
+            app.MapRazorPages();
 
-			app.Run();
-		}
-	}
+            await app.SeedRolesAndUsersAsync();
+
+            app.Run();
+        }
+    }
 }
