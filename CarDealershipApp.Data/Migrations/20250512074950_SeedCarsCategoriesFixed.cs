7be5f4047d4890239b8e5b6c3fc41ce9ab87eeed
okay osman <okay03@abv.bg>
2025-05-12 10:51:41 +0300

    Category SeedData of Cars fixed

---
+++ CarDealershipApp.Data/Migrations/20250512074950_SeedCarsCategoriesFixed.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarDealershipApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCarsCategoriesFixed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "4e32b02a-046c-40be-bfeb-327c900e6bb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "f568a3a5-bfeb-464a-8619-32cc4dad0c58", "AQAAAAIAAYagAAAAEFKXRKRu2tvjXpuXCN9Shm/pwylM0R7exxPO+Tu9KRvfRKHN5ZLbIZc6ltPLS3WE2A==", "7680d3ea-f6cf-47a9-998d-cf28a8f741e8" });
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 8);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CategoryId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "CategoryId",
+                value: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "4e32b02a-046c-40be-bfeb-327c900e6bb9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c34a3505-9260-45d1-9b2c-d5e74c47798c", "AQAAAAIAAYagAAAAEKoEod11N5fVkdAS/xuTLtLSDzVoDNmFsWYp1rLDGTwTS1aXNuQMt0WccWKcgSjpig==", "4c903c26-5a86-4e75-9948-2e9905318ddb" });
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CategoryId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CategoryId",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CategoryId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "CategoryId",
+                value: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "CategoryId",
+                value: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "CategoryId",
+                value: 3);
+        }
+    }
+}
