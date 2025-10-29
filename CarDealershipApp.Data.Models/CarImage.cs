7e6785f658ab18f0ae5be67d935d0b0a80f12e21
okicikk <okay03@abv.bg>
2025-10-29 21:02:22 +0200

    CarImage class added

---
+++ CarDealershipApp.Data.Models/CarImage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealershipApp.Data.Models
+{
+    public class CarImage
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FilePath { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(Car))]
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+    }
+
+}
